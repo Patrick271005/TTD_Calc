e6c0b381b945d5198bd6a707c043526a4b75d0d2
Patrick Flanagan <G00431255@atu.ie>
2025-03-24 19:08:14 +0000

    Completed Feature 2

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,7 +2,20 @@ public class Calculator
 {
     public int add(int firstNum, int secondNum)
     {
-        int result = firstNum + secondNum;
-        return result;
+        long result =(long)firstNum+secondNum;
+        if(result > Integer.MAX_VALUE)
+        {
+            throw new IllegalArgumentException("Input is too large");
+        }
+        return (int)result;
+    }
+    public int sub(int firstNum, int secondNum)
+    {
+        long result =(long)firstNum-secondNum;
+        if(result <0)
+        {
+            throw new IllegalStateException("Input cannot be negative");
+        }
+        return (int)result;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculatorTest {
     Calculator myCalc;
@@ -15,5 +16,24 @@ public class CalculatorTest {
     {
         assertEquals(8,myCalc.add(4,4));
     }
+    @Test
+    void testAddFail()
+    {
+        Exception ex=assertThrows(IllegalArgumentException.class,()->{myCalc.add(Integer.MAX_VALUE,4);});
+        assertEquals("Input is too large", ex.getMessage());
+    }
+    @Test
+    void testSubSuccess()
+    {
+        assertEquals(4,myCalc.sub(6,2));
+    }
+    @Test
+    void testSubFail()
+    {
+        Exception ex=assertThrows(IllegalStateException.class,()->{myCalc.sub(Integer.MIN_VALUE,2);});
+        assertEquals("Input cannot be negative", ex.getMessage());
+    }
+
+
 
 }
